709414d00bc2b8741314432399b5c48d7af0eee8
Ogafarov <157960271+Ogafarov@users.noreply.github.com>
2024-01-28 13:33:54 +0100

    Add files via upload
    I have upload Homework.

---
+++ Lesson2HomeWork.java
@@ -0,0 +1,59 @@
+public class Lesson2HomeWork {
+    public static void main(String[] args) {
+        double priceOfProductA = 1000;
+        double priceOfProductB = 500;
+        double discountOfProduct = 100;
+        double discountOfValue = priceOfProductA + priceOfProductB - discountOfProduct;
+        double NewPrice = priceOfProductA + priceOfProductB - discountOfValue;
+
+        System.out.println("discountOfValue" + discountOfValue);
+        System.out.println("NewPrice" + NewPrice);
+
+        int fisrt = 0;
+        int two = 1;
+        int three = 2;
+        int four = 3;
+        int five = 4;
+        int six = 5;
+        int seven = 6;
+        int eight = 7;
+        int nine = 8;
+        int ten = 9;
+        int summe = fisrt + two + three + four + five + six + seven + eight + nine + ten;
+        summe /= 10;
+        System.out.println("summe"+summe);
+
+        boolean myBoolean = false;
+        byte myByte = 126;
+        short myShort = 15468;
+        int myInt = 4242455;
+        long myLong = 0L;
+        float myFloat = 24f;
+        double myDouble = 0.0d;
+        char myChar = 65535;
+
+        System.out.println("myBoolen" + myBoolean);
+        System.out.println("myByte"+ myByte);
+        System.out.println("myshort" + myShort);
+        System.out.println("myInt" + myInt);
+        System.out.println("myLong"+myLong);
+        System.out.println("myFloat" + myFloat);
+        System.out.println("myDouble"+ myDouble);
+        System.out.println("myChar" + myChar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
